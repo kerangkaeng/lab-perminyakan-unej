8c054abce69ab580b32e80ca506c61b619e3bc50
Ilham Maulana Hermansyah <ilhamherman2178@gmail.com>
2026-09-05 14:38:37 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.supabase.co" },
+      { protocol: "https", hostname: "picsum.photos" },
     ],
   },
 };
